7ec36f0fe89436743fa83a4688392ebd421e7aa2
VeronikaAsenova <veronikaasenova0@gmail.com>
2015-06-13 15:47:53 +0300

    My  Task 1 of homework 3

---
+++ Task 1 of homework 3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 200;
+            int b = 500;
+
+            int result = a + b;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
