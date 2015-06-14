1042cd092487f781ea767653b57d2f049a4fb3dd
VeronikaAsenova <veronikaasenova0@gmail.com>
2015-06-14 16:55:00 +0300

    My homework 3 with 8 tasks

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a value for a:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a value for ha:");
+            float ha = float.Parse(Console.ReadLine());
+            float result = (a * ha) / 2;
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Homework5/Homework5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            Console.WriteLine(str1 + "_" + str2);
+
+        }
+    }
+}
