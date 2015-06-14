7de5e11a34b96889f3e70481160f5132dd31c3e2
VeronikaAsenova <veronikaasenova0@gmail.com>
2015-06-14 10:45:12 +0300

    My Task 3 of homework 3 triangle

---
+++ Task 3 of hm3 triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =
+                Console.WriteLine();
+                Console.ReadLine();
+
+            int ha = 
+                Console.ReadLine();
+                
+
+
+             int result = (a * ha) / 2;
+            Console.WriteLine(result);
+
+        }
+    }
+}
