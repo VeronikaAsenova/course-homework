183ec98ebcd82b21d82c1dfc2d429efa17642449
VeronikaAsenova <veronikaasenova0@gmail.com>
2015-06-14 12:56:22 +0300

    my good task 3

---
+++ Task3 of hm3 triangle2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=int.Parse(Console.ReadLine());
+            int ha = int.Parse(Console.ReadLine());
+            int result = (a * ha) / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
