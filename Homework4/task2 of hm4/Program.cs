fb1ebbf04de47660d38801f58ddc1dcb3ea0a2b6
VeronikaAsenova <veronikaasenova0@gmail.com>
2015-06-20 10:45:33 +0300

    My Homework 4 with 3 tasks

---
+++ Homework4/task2 of hm4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_of_hm4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your score:");
+            int score = int.Parse(Console.ReadLine());
+
+            if (score>1 && score<=3)
+            {
+                Console.WriteLine(score*5);
+
+            }
+          else if(score>4 && score<=6)
+            {
+                Console.WriteLine(score*10);
+            }
+            else if (score>7 && score<=9)
+            {
+                Console.WriteLine(score*50);
+            }
+        }
+
+        
+
+	}
+	
+		 
+	}
+    
+
---
+++ Homework4/task3 of hm4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_of_hm4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a letter or string:");
+            string str=(Console.ReadLine());
+
+            switch (str)
+            {
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "10":
+                    Console.WriteLine("Valid card");
+                    break;
+
+                case "J":
+                case "Q":
+                case "K":
+                case "A":
+                    Console.WriteLine("Valid card");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid card");
+                    break;
+
+
+            }
+        }
+    }
+}
